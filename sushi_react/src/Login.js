e3e1e5c625446dacf7fe1007dd7c2e106b15252a
React0128 <top.master716@gmail.com>
2020-01-28 03:07:13 -0600

    UI: login page created.

---
+++ sushi_react/src/Login.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Login.css";
+import KEY from "./assests/images/key.png";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Login-container">
+        <div className="Login-logo"></div>
+        <div className="Login-box">
+          <div className="Login-slogo">
+            <img className="Img-key" src={KEY} alt="key"></img>
+          </div>
+          <UserName></UserName>
+        </div>
+      </div>
+    );
+  }
+}
+
+class UserName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="UserName-container">
+        <div className="input-container">
+          <input
+            className="input-username"
+            type="text"
+            name="username"
+            placeholder="USERNAME"
+          />
+        </div>
+        <div className="input-container">
+          <input
+            className="input-username"
+            type="password"
+            name="password"
+            placeholder="PASSWORD"
+          />
+        </div>
+        <div className="input-container">
+          <input
+            className="btn-login"
+            type="button"
+            name="login"
+            placeholder="LOGIN"
+            value="LOGIN"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
